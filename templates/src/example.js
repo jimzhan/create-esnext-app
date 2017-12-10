af2ec239d5a27ed5f7d3a2bc5a580cce58d154ea
Jim Zhan <jim.zhan@me.com>
2017-12-10 20:07:49 +0800

    [WIP] added basic jest supports with sample test specs

---
+++ templates/src/example.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const root = 'https://jsonplaceholder.typicode.com/users'
+
+export const find = async () => {
+  const response = await axios.get(`${root}/`)
+  return response.data
+}
+
+export const findById = async id => {
+  const response = await axios.get(`${root}/${id}`)
+  return response.data
+}
+
+export const create = async () => {
+  axios.post(root, {})
+}
+
+export const update = async () => {
+  axios.put(root, {})
+}
+
+export const remove = async () => {
+  axios.delete(root, {})
+}
---
+++ templates/tests/example.spec.js
@@ -0,0 +1,13 @@
+import * as User from '../src/example'
+
+const URL = 'https://jsonplaceholder.typicode.com'
+
+it(`find users from ${URL}`, async () => {
+  const users = await User.find()
+  expect(users.length).toBeGreaterThan(3)
+})
+
+it(`find specific user from ${URL}`, async () => {
+  const user = await User.findById(3)
+  expect(user.id).toEqual(3)
+})
