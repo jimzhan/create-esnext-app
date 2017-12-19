9eb8223d201e079bc22b7ed52244f3998c4cf378
Jim Zhan <jim.zhan@me.com>
2017-12-19 15:01:59 +0800

    refactor(structure): re-structured `react` template folder structures

--- templates/react/src/index.js
+++ templates/react/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './tasks/registerServiceWorker'
+import './styles/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
