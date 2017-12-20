640ee1461d9311366fa628101880af3a5e697ee3
Jim Zhan <jim.zhan@me.com>
2017-12-20 19:37:59 +0800

    style(prompt): added emoji to prompt message

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { Basic, Fastify, React, MobX } = packages
 const createESNextApp = answers => {
   const { name, template } = answers
   console.log(
-    chalk`{green ●} Start creating ESNext application <type: ${template}>: ${name}`
+    chalk`{green ●} 🤖 Start creating ESNext application <type: ${template}>: ${name}`
   )
 
   switch (answers.template) {
