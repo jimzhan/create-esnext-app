ca3db405586120049a2d028f76f5bd9999427c81
Jim Zhan <jim.zhan@me.com>
2017-12-28 17:38:26 +0800

    refactor(template): added `react-hot-loader` for root AppContainer

--- templates/mobx/src/index.js
+++ templates/mobx/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'mobx-react'
+import { AppContainer } from 'react-hot-loader'
+
 import { registerServiceWorker } from 'tasks'
 import { Home } from 'containers'
 import Store from 'stores'
-import './assets/styles/index.css'
 
 ReactDOM.render(
-  <Provider store={new Store()}>
-    <Home />
-  </Provider>,
+  <AppContainer>
+    <Provider store={new Store()}>
+      <Home />
+    </Provider>
+  </AppContainer>,
   document.getElementById('root')
 )
 registerServiceWorker()
