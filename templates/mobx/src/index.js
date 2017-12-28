0a9ad30c2ce431f2d4d29e0e3f22ee1f2052073b
Jim Zhan <jim.zhan@me.com>
2017-12-28 20:59:55 +0800

    refactor(template): switched back to `react-router`

--- templates/mobx/src/index.js
+++ templates/mobx/src/index.js
@@ -2,17 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'mobx-react'
 import { AppContainer } from 'react-hot-loader'
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import { registerServiceWorker } from 'tasks'
 import { Home } from 'containers'
 import Store from 'stores'
 
-ReactDOM.render(
-  <AppContainer>
-    <Provider store={new Store()}>
-      <Home />
-    </Provider>
-  </AppContainer>,
-  document.getElementById('root')
-)
-registerServiceWorker()
+const renderApp = () => {
+  const store = new Store()
+  const browserHistory = createBrowserHistory()
+  const routeStore = new RouterStore()
+  const history = syncHistoryWithStore(browserHistory, routeStore)
+
+  ReactDOM.render(
+    <AppContainer>
+      <Router history={history}>
+        <Provider store={store} routing={routeStore}>
+          <Home />
+        </Provider>
+      </Router>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+  registerServiceWorker()
+}
+
+renderApp()
